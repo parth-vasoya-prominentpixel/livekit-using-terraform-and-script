4383b10934724a12ff16d9a06298addc3667639b
parth-vasoya-prominentpixel <parth.vasoya@prominentpixel.com>
2025-12-19 12:26:32 +0530

    Added addons and some fixes

--- resources/eks_cluster.tf
+++ resources/eks_cluster.tf
@@ -3,12 +3,9 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = var.cluster_version
-  
-  cluster_endpoint_config = {
-    public_access = true
-  }
+  cluster_name                   = local.cluster_name
+  cluster_version                = var.cluster_version
+  cluster_endpoint_public_access = true
   
   enable_cluster_creator_admin_permissions = true
 
