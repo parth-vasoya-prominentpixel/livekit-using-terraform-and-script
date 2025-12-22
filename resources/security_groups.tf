b122b5026775893b6b0565a885162d8d116143ad
parth-vasoya-prominentpixel <parth.vasoya@prominentpixel.com>
2025-12-22 15:59:19 +0530

    Changes in livekit deploy

--- resources/security_groups.tf
+++ resources/security_groups.tf
@@ -40,30 +40,3 @@ resource "aws_security_group" "sip_traffic" {
     Name = "${local.eks_name}-sip-twilio"
   })
 }
-
-# Additional security group rule for EKS to access Redis
-resource "aws_security_group_rule" "eks_to_redis" {
-  type                     = "ingress"
-  from_port                = 6379
-  to_port                  = 6379
-  protocol                 = "tcp"
-  source_security_group_id = module.eks_al2023.cluster_security_group_id
-  security_group_id        = module.redis.security_group_id
-  description              = "Allow EKS cluster to access Redis"
-
-  depends_on = [module.eks_al2023, module.redis]
-}
-
-# Additional security group rules for private subnets to access Redis
-resource "aws_security_group_rule" "private_subnets_to_redis" {
-  count             = length(module.vpc.private_subnets_cidr_blocks)
-  type              = "ingress"
-  from_port         = 6379
-  to_port           = 6379
-  protocol          = "tcp"
-  cidr_blocks       = [module.vpc.private_subnets_cidr_blocks[count.index]]
-  security_group_id = module.redis.security_group_id
-  description       = "Allow private subnet ${count.index + 1} to access Redis"
-
-  depends_on = [module.redis, module.vpc]
-}
